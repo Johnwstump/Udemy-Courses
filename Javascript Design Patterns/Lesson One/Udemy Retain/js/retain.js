e63da0eab812ea2ef55f99783647c1cd8da14c95
Johnwstump <johnwstump@outlook.com>
2018-01-16 03:29:06 -0500

    Completed Lesson One
    Javascript Design Patterns Lesson One Pt 18 - 27.
    Added date functionality to Udemy Retain example.

---
+++ Javascript Design Patterns/Lesson One/Udemy Retain/js/retain.js
@@ -0,0 +1,70 @@
+$(function(){
+
+    var model = {
+        init: function() {
+            if (!localStorage.notes) {
+                localStorage.notes = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.notes);
+            data.push(obj);
+            localStorage.notes = JSON.stringify(data);
+        },
+        getAllNotes: function() {
+            return JSON.parse(localStorage.notes);
+        }
+    };
+
+
+    var octopus = {
+        addNewNote: function(noteStr) {
+            model.add({
+                content: noteStr,
+				date: Date.now(),
+            });
+            view.render();
+        },
+
+        getNotes: function() {
+            return model.getAllNotes().reverse();
+        },
+
+        init: function() {
+            model.init();
+            view.init();
+        }
+    };
+
+
+    var view = {
+        init: function() {
+            this.noteList = $('#notes');
+            var newNoteForm = $('#new-note-form');
+            var newNoteContent = $('#new-note-content');
+            newNoteForm.submit(function(e){
+                octopus.addNewNote(newNoteContent.val());
+                newNoteContent.val('');
+                e.preventDefault();
+            });
+            view.render();
+        },
+        render: function(){
+            var htmlStr = '';
+            octopus.getNotes().forEach(function(note){
+				var d = new Date(note.date);
+                htmlStr += '<li class="note">'+
+                        note.content + 
+						'<p class=note.date>' + 
+						d.toLocaleDateString() + 
+						'<br>' +
+						d.toLocaleTimeString() + 
+						'<\p>' +
+                    '</li>';
+            });
+            this.noteList.html( htmlStr );
+        }
+    };
+
+    octopus.init();
+});
