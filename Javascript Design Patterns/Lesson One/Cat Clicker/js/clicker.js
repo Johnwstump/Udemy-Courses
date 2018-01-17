af359b314745f0e5a8e1aacdc82c783e3619b200
Johnwstump <johnwstump@outlook.com>
2018-01-17 09:09:43 -0500

    Added Admin Functionality
    Javascript Design Patterns Lesson 2 Pt. 12 - 13

--- Javascript Design Patterns/Lesson One/Cat Clicker/js/clicker.js
+++ Javascript Design Patterns/Lesson One/Cat Clicker/js/clicker.js
@@ -1,77 +1,142 @@
 
 	var domain = {
 		CAT_NAMES : ["Jim", "Sally", "Fluffy", "Benjamin", "Mint"],
+		CAT_URLS : ["http://kittentoob.com/wp-content/uploads/2018/01/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg", 
+		"https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr06/4/16/enhanced-23752-1396643152-1.jpg?downsize=715:*&output-format=auto&output-quality=auto", 
+		"https://d1wn0q81ehzw6k.cloudfront.net/additional/thul/media/0eaa14d11e8930f5?w=400&h=400", 
+		"https://cdn.shopify.com/s/files/1/0673/5325/files/LadyDinahs_Cat_Cafe_Alice-Block_2048x.jpg?v=1510928396",
+		"https://d3n8a8pro7vhmx.cloudfront.net/taxpayers/pages/679/attachments/original/1499663166/4-ways-cheer-up-depressed-cat.jpg?1499663166"],
 		cats : new Map(),
-		currCat : null
+		currCat : null,
+		showAdmin : false
 	}
 	
 	
 	var controller = {	
-		IMAGE_FOLDER : "Image",
-
-
+		dataChanged : false,
 		
 		init : function(){
 			for (var i = 0; i < domain.CAT_NAMES.length; i++){
-				listView.addNameToList(domain.CAT_NAMES[i]);
-				domain.cats.set(domain.CAT_NAMES[i], {
+				var cat = {
+					key : cat + i,
+					name : domain.CAT_NAMES[i],
 					counter : 0,
-					index: i
-				});
+					url : domain.CAT_URLS[i]
+				};
+				domain.cats.set(cat.key, cat);
 			}
 			
 			catView.init();
+			adminView.init();
 			listView.init();
-			if (domain.CAT_NAMES.length > 0){
-				this.changeCat(domain.CAT_NAMES[0]);
+			
+			if (domain.currCat == null){
+				this.changeCat(domain.cats.keys().next().value);
 			}
 		},
 	    
-		changeCat : function(name){
-			domain.currCat = domain.cats.get(name);
-
-			listView.highlight(name);
-			catView.setCounterVal(domain.currCat.counter);
-			catView.displayCatPic(this.IMAGE_FOLDER + "/cat" + (domain.currCat.index + 1) + ".jpg");
-			catView.displayCatName(name);
+		changeCat : function(key){
+			domain.currCat = domain.cats.get(key);
+			listView.render();
+			catView.render();
+			adminView.render();
+		},
+		
+		updateCurrentCat : function(){
+			domain.currCat.name = adminView.getAdminName();
+			domain.currCat.counter = adminView.getAdminCounter();
+			domain.currCat.url = adminView.getAdminUrl();
+			controller.setDataChanged(true);
+			listView.render();
+			catView.render();
 		},
 		
 		incrementCounter : function(){
 			if (domain.currCat != null){
 				domain.currCat.counter++;
-				catView.setCounterVal(domain.currCat.counter);
+				catView.render();
+				adminView.render();
 			}
+		},
+		
+		showAdmin : function(){
+			return domain.showAdmin;
+		},
+		
+		toggleShowAdmin : function(){
+			domain.showAdmin = !domain.showAdmin;
+			adminView.render();
 		},
 		
 		setCurrCat : function(cat){
 			domain.currCat = cat;
+		},
+		
+		getCurrCat : function(){
+			return domain.currCat;
+		},
+		
+		getCats : function(){
+			return domain.cats;
+		},
+		
+		isDataChanged : function(){
+			return this.dataChanged;
+		},
+		
+		setDataChanged : function(changed){
+			this.dataChanged = changed;
 		}
 		
 	}
 	
 	var listView = {
+		// Constaants
 		DEFAULT_TEXT_COLOR : "black",
 		HIGHLIGHT_COLOR : "blue",
+		// The id (cat.name) of the currently highlighted list item
 		currHighlight : "",
+		// The ul which contains the list items
 		catList : null,
 		
 		init : function(){
 			this.catList = document.getElementById('catList');
+			this.catList.innerHTML = "";
+			
+			for (var cat of controller.getCats().values()){
+				this.addCatToList(cat);
+			}
+			controller.setDataChanged(false);
 		},
 			
-		addNameToList : function(name){
+		addCatToList : function(cat){
+			// Create list element for this cat
 			var catListing = document.createElement('li');
 			catListing.className = 'listedCats';
-			catListing.id = name;
-			catListing.textContent = name;
+			catListing.id = cat.key;
+			catListing.textContent = cat.name;
 			
-			catListing.addEventListener('click', (function(name){
+			// Create event listener to refresh views on change
+			catListing.addEventListener('click', (function(key){
 				return function(){
-					controller.changeCat(name);
+					controller.changeCat(key);
 				};
-			})(name));
+			})(cat.key));
 			
-			catList.append(catListing);
+			// Add listing to list
+			this.catList.append(catListing);
+		},
+		
+		render : function(){
+			if (controller.isDataChanged()){
+				this.init();
+			}
+			//Highlight list element associated with currently selected cat
+			this.highlight(controller.getCurrCat().key);
+		},
+		
+		getSelected : function(){
+			return this.currHighlight;
 		},
 		
 		highlight : function(name){
@@ -103,6 +168,15 @@
 			});
 		},
 		
+		render : function(){
+			var cat = controller.getCurrCat();
+			if (cat != null){
+				this.displayCatName(cat.name);
+				this.displayCatPic(cat.url);
+				this.setCounterVal(cat.counter);
+			}
+		},
+		
 		displayCatName : function(name){
 			catName.textContent = name;
 		},
@@ -120,4 +194,62 @@
 		}
 	}
 
+	var adminView = {
+		adminForm : null,
+		adminToggleButton : null,
+		adminSubmit : null,
+		changeButton: null,
+		adminName : null,
+		adminUrl : null,
+		adminCounter : null,
+		
+		init : function(){
+			this.adminForm = document.getElementById('adminForm');
+			this.adminToggleButton = document.getElementById('adminToggleButton');
+			this.adminToggleButton.addEventListener('click', function(){
+				controller.toggleShowAdmin();
+			});
+			
+			this.adminSubmit = document.getElementById('adminFormSubmit');
+			this.adminName = document.getElementById('adminCatName');
+			this.adminUrl = document.getElementById('adminCatImage');
+			this.adminCounter = document.getElementById('adminCatCount');
+			
+			$('#adminForm').submit(function(ev) {
+				ev.preventDefault(); 
+				controller.updateCurrentCat();
+			});
+		},
+		
+		
+		render : function(){
+			if (controller.showAdmin()){
+				var cat = controller.getCurrCat();
+				this.adminName.value  = cat.name;
+				this.adminUrl.value  = cat.url;
+				this.adminCounter.value  = cat.counter;
+				this.adminForm.style.visibility = "visible";
+				this.adminSubmit.style.visibility = "visible";
+			}
+			else{
+				this.adminForm.style.visibility = "hidden";
+				this.adminSubmit.style.visibility = "hidden";
+			}
+			
+		},
+		
+		getAdminCounter : function(){
+			return Number(this.adminCounter.value);
+		},
+		
+		getAdminUrl : function(){
+			return String(this.adminUrl.value);
+		},
+		
+		getAdminName : function(){
+			return String(this.adminName.value);
+		}
+	}
+
 	controller.init();
+	
