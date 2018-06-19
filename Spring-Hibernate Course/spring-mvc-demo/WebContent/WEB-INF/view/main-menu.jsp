6a9eb0e987f58aa29c2b22bf22b4a10043e617eb
Johnwstump <johnwstump@outlook.com>
2018-06-19 14:42:02 -0400

    Added Spring-MVC Demo
    Displays basic MVC setup with JSP views, controllers, and request
    mapping.

---
+++ Spring-Hibernate Course/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	
+	<body>
+		<h2>Spring MVC Demo - Home Page</h2>
+	
+		<hr>
+		
+		<a href="hello/showForm">Show Hello World Form</a>
+		<a href="goodbye/showForm">Show Goodbye World Form</a>
+	</body>
+
+</html>
---
+++ Spring-Hibernate Course/spring-mvc-demo/src/com/johnwstump/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,43 @@
+package com.johnwstump.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+	// Show the initial HTML form
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// Process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm(){
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String nameToCaps(HttpServletRequest request, Model model) {
+		String name = request.getParameter("studentName");
+		name = name.toUpperCase();
+		String result = String.format("HELLO, %s", name);
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+		name = name.toUpperCase();
+		String result = String.format("HELLO, %s!", name);
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+}
