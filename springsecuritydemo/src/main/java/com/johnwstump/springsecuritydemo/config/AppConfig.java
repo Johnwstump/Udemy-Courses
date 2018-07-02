f4b9b2fd0918bf1d179ce7e0af40f75a90e43fa3
Johnwstump <johnwstump@outlook.com>
2018-07-02 18:57:43 -0400

    Initial commit
    Created simple web mvc demo with java config files.

---
+++ springsecuritydemo/src/main/java/com/johnwstump/springsecuritydemo/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.johnwstump.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.johnwstump.springsecuritydemo")
+public class AppConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+
+}
